4606cbb186723232f747cd68e2c39a805a35ffdf
thiagola92 <thiagola92@gmail.com>
2022-10-22 15:49:37 -0300

    Update colors from labels

--- labels.tf
+++ labels.tf
@@ -2,7 +2,7 @@ resource "github_issue_label" "bug" {
   for_each    = toset(data.github_repositories.la_catalog.names)
   repository  = each.key
   name        = "bug"
-  color       = "d73a4a"
+  color       = "FBCA04"
   description = "Algo que funcionava quebrou"
 }
 
@@ -10,7 +10,7 @@ resource "github_issue_label" "enhancement" {
   for_each    = toset(data.github_repositories.la_catalog.names)
   repository  = each.key
   name        = "enhancement"
-  color       = "a2eeef"
+  color       = "1D76DB"
   description = "Algo que funciona pode ser melhorado"
 }
 
@@ -18,7 +18,7 @@ resource "github_issue_label" "feature" {
   for_each    = toset(data.github_repositories.la_catalog.names)
   repository  = each.key
   name        = "feature"
-  color       = "a2eeef"
+  color       = "C5DEF5"
   description = "Algo novo que pode ser implementado"
 }
 
@@ -26,7 +26,7 @@ resource "github_issue_label" "problem" {
   for_each    = toset(data.github_repositories.la_catalog.names)
   repository  = each.key
   name        = "problem"
-  color       = "a2eeef"
+  color       = "F9D0C4"
   description = "Algo que não sabemos como resolver"
 }
 
@@ -34,7 +34,7 @@ resource "github_issue_label" "refactor" {
   for_each    = toset(data.github_repositories.la_catalog.names)
   repository  = each.key
   name        = "refactor"
-  color       = "a2eeef"
+  color       = "0E8A16"
   description = "Algum código que pode ser reescrito"
 }
 
@@ -42,7 +42,7 @@ resource "github_issue_label" "clean" {
   for_each    = toset(data.github_repositories.la_catalog.names)
   repository  = each.key
   name        = "clean"
-  color       = "a2eeef"
+  color       = "C2E0C6"
   description = "Algum texto/documentação que pode ser reescrito"
 }
 
@@ -50,6 +50,6 @@ resource "github_issue_label" "question" {
   for_each    = toset(data.github_repositories.la_catalog.names)
   repository  = each.key
   name        = "question"
-  color       = "d876e3"
+  color       = "D4C5F9"
   description = "Alguma pergunta"
 }
