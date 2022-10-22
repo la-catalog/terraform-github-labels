722116cf71349d410c7c0c9a474bffa57f01fe5b
thiagola92 <thiagola92@gmail.com>
2022-10-22 01:06:14 -0300

    Update labels and descriptions

--- labels.tf
+++ labels.tf
@@ -3,15 +3,7 @@ resource "github_issue_label" "bug" {
   repository  = each.key
   name        = "bug"
   color       = "d73a4a"
-  description = "Something isn't working"
-}
-
-resource "github_issue_label" "documentation" {
-  for_each    = toset(data.github_repositories.la_catalog.names)
-  repository  = each.key
-  name        = "documentation"
-  color       = "0075ca"
-  description = "Improvements or additions to documentation"
+  description = "Algo que funcionava quebrou"
 }
 
 resource "github_issue_label" "enhancement" {
@@ -19,15 +11,39 @@ resource "github_issue_label" "enhancement" {
   repository  = each.key
   name        = "enhancement"
   color       = "a2eeef"
-  description = "New feature or request"
+  description = "Algo que funciona pode ser melhorado"
 }
 
-resource "github_issue_label" "help_wanted" {
+resource "github_issue_label" "feature" {
   for_each    = toset(data.github_repositories.la_catalog.names)
   repository  = each.key
-  name        = "help wanted"
-  color       = "008672"
-  description = "Extra attention is needed"
+  name        = "feature"
+  color       = "a2eeef"
+  description = "Algo novo que pode ser implementado"
+}
+
+resource "github_issue_label" "problem" {
+  for_each    = toset(data.github_repositories.la_catalog.names)
+  repository  = each.key
+  name        = "problem"
+  color       = "a2eeef"
+  description = "Algo que não sabemos como resolver"
+}
+
+resource "github_issue_label" "refactor" {
+  for_each    = toset(data.github_repositories.la_catalog.names)
+  repository  = each.key
+  name        = "refactor"
+  color       = "a2eeef"
+  description = "Algum código que pode ser reescrito"
+}
+
+resource "github_issue_label" "clean" {
+  for_each    = toset(data.github_repositories.la_catalog.names)
+  repository  = each.key
+  name        = "clean"
+  color       = "a2eeef"
+  description = "Algum texto/documentação que pode ser reescrito"
 }
 
 resource "github_issue_label" "question" {
@@ -35,5 +51,5 @@ resource "github_issue_label" "question" {
   repository  = each.key
   name        = "question"
   color       = "d876e3"
-  description = "Further information is requested"
+  description = "Alguma pergunta"
 }
